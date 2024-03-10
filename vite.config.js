0074f96764a006b8cc00c80cab0847de33568c19
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-03-10 15:35:00 +0700

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"belajar_fetchAPI_web_onlineshopp",
+  base:"/belajar_fetchAPI_web_onlineshopp/",
   plugins: [react()],
 })
